0de9027fddf454589dcf0c6d2746f5e14cb3921d
Олег Цыплаков <olegtsy@mail.ru>
2023-10-27 15:17:22 +0400

    hot reload config file

--- iternal/env.go
+++ iternal/env.go
@@ -1,28 +1,41 @@
 package configure
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
 
 type env struct {
-	AppEnv              string   `mapstructure:"APP_ENV"`
-	InputFolder         string   `mapstructure:"Input_Folder"`
-	OutputFolder        string   `mapstructure:"Output_Folder"`
-	FileExtensions      []string `mapstructure:"File_Extensions"`
-	EventsToListen      []string `mapstructure:"Events_To_Listen"`
-	WhiteColorProximity float64  `mapstructure:"White_Color_Proximity"`
+	AppEnv         string   `mapstructure:"APP_ENV"`
+	InputFolder    string   `mapstructure:"Input_Folder"`
+	OutputFolder   string   `mapstructure:"Output_Folder"`
+	FileExtensions []string `mapstructure:"File_Extensions"`
+	EventsToListen []string `mapstructure:"Events_To_Listen"`
 }
 
 func newEnv() *env {
 	env := env{}
-	viper.SetConfigFile(".env")
+	env.loadConfiguration()
+	return &env
+}
 
+func (env *env) loadConfiguration() {
+	viper.SetConfigFile(".env")
 	err := viper.ReadInConfig()
+
 	if err != nil {
 		log.Fatal("Can't find the file .env : ", err)
 	}
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+		viper.Unmarshal(&env)
+		fmt.Println("env.FileExtensions:", env.EventsToListen)
+	})
 
 	err = viper.Unmarshal(&env)
 	if err != nil {
@@ -32,6 +45,4 @@ func newEnv() *env {
 	if env.AppEnv == "development" {
 		log.Println("The App is running in development env")
 	}
-
-	return &env
 }
